26801821717e1bf4768c2b815c35f57e22c530f0
techerfan <techerfan@gmail.com>
2021-08-25 12:19:52 +0430

    add config and related methods

--- panda.go
+++ panda.go
@@ -1,10 +1,73 @@
 package panda
 
-type Panda struct{}
+import (
+	"log"
+	"net/http"
 
-type Config struct {
+	"github.com/gorilla/websocket"
+)
+
+const DefaultWebSocketPath = "/ws"
+
+var Upgrader = websocket.Upgrader{
+	ReadBufferSize:    0,
+	WriteBufferSize:   0,
+	EnableCompression: true,
+	CheckOrigin: func(r *http.Request) bool {
+		return true
+	},
 }
 
-func New() {
+type CommunicationType int
+
+const (
+	JSON CommunicationType = iota
+	BINARY
+	// not implemented yet
+	XML
+)
+
+type App struct {
+	config Config
+	client []*client
+}
+
+type Config struct {
+	ServerAddress     string
+	WebSocketPath     string
+	CommunicationType CommunicationType
+}
+
+func NewApp(config Config) *App {
+	app := &App{
+		config: config,
+	}
+
+	if config.WebSocketPath == "" {
+		app.config.WebSocketPath = DefaultWebSocketPath
+	}
+
+	return app
+}
+
+func (a *App) serveWs(rw http.ResponseWriter, r *http.Request) {
+	conn, err := Upgrader.Upgrade(rw, r, nil)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	a.client = append(a.client, &client{
+		conn: conn,
+	})
+}
+
+func (a *App) Serve() {
+	http.HandleFunc(a.config.WebSocketPath, func(rw http.ResponseWriter, r *http.Request) {
+		a.serveWs(rw, r)
+	})
+	http.ListenAndServe(a.config.ServerAddress, nil)
+}
+
+func (a *App) Channel() {
 
 }
